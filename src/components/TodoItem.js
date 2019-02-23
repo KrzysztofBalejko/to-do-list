efb595ebc07f62e5cec0f67eb1bae89c63a09257
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-23 18:30:31 +0000

    Toggle complete. Added markComplete function which takes todo id.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+
+  getStyle = () => {
+    return {
+      background: '#77DD77',
+      padding: '10px',
+      margin: '5px',
+      opacity: '0.7',
+      borderBottom: '1px #fff dotted',
+      textDecoration: this.props.todo.completed ?
+      'line-through' : 'none'
+    }
+  }
+
+
+
   render() {
+
+    const { id, title } = this.props.todo;
+
     return (
-      <p>{ this.props.todo.title }</p>
+      <div style={this.getStyle()}>
+      <p>
+        <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {''}
+        { title }
+      </p>
+      </div>
     );
   }
 
@@ -14,5 +37,6 @@ class TodoItem extends Component {
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 }
+
 
 export default TodoItem ;
