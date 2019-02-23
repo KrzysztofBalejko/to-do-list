35511046cd69d8ebc61019424d3267b3bf36f836
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-23 15:37:09 +0000

    Adding state and props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import './App.css';
 import Todos from './components/Todos.js';
 
 class App extends Component {
+  state = {
+    todos: [
+    {
+      id: 1,
+      title: 'Walk the dog',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Make a bed',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Cook a dinner',
+      completed: false
+    },
+   ]
+  }
+
   render() {
+
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
