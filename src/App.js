858c6e51672bbbdd5732dd0046ebf4c5a2717021
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-23 15:19:25 +0000

    Importing newly created class Todos into App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>App</h1>
+        <Todos />
       </div>
     );
   }
