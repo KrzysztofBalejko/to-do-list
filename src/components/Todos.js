417c031ebf7dc4b4f17fa4dbe4e3b8e06b2c137d
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-23 16:14:29 +0000

    Created TodoItem class which renders todo.title

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem.js';
+
+
 
 class Todos extends Component {
-
   render() {
-    console.log(this.props.todos)
     return this.props.todos.map((todo) =>(
-      <h3>{ todo.title }</h3>
+      <TodoItem todo={todo}/>
     ));
 
   }
